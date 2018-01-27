b03ef4bb639f23a03f6564726e66e18b9e75b07c
CodeFace <i@codeface.cn>
2018-01-27 13:46:57 +0800

    fix setResult

--- SatelliteContract.sol
+++ SatelliteContract.sol
@@ -98,7 +98,7 @@ contract SatelliteContract {
         if (block.number - lastBlockHeight < voteGap) {
           return false;
         }
-        bytes32 blockhash = block.blockhash(block.number);
+        bytes32 blockhash = block.blockhash(block.number-1);
         uint256 hash = uint256(keccak256(blockhash, _randomSeed));
         bytes memory encode = new bytes(resultLength);
         for(uint8 i = 0; i < resultLength; i++) {
